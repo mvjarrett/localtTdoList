4deb124342fd103721fcfd69684abaf355b02832
Matt <mvjarrett92@gmail.com>
2020-07-15 02:12:40 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+// Check Off Todos By Clicking
+$('ul').on('click', 'li', function() {
+	$(this).toggleClass('completed');
+});
+
+$('ul').on('click', 'span', function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val('');
+		$('ul').append('<li><span><i class="fa fa-trash"></i> </span>' + todoText + '</li>');
+	}
+});
+
+
+$("#plus").click(function(){
+	$("input[type='text']").fadeToggle();
+})
+
+
+
+
+
+
+
+
+
+
+// input = document.getElementById('input');
+
+// function ph() {
+// 	input.setAttribute('placeholder', 'Add New Todo');
+// }
+
+// function phr() {
+// 	input.setAttribute('placeholder', '');
+// }
+
+// input.addEventListener('mouseover', phr);
+// input.addEventListener('mouseout', ph);
+
+
